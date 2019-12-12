f28dc7b8f87b7f7b67afd752392a64c292dfef4f
qwe735689793 <735689793@qq.com>
2019-12-12 17:34:02 +0800

    Eureka p6p7章节学习 代码提交

--- springcloud-provider-dept-8001/src/main/java/com/springcloud/DeptProcider_8001.java
+++ springcloud-provider-dept-8001/src/main/java/com/springcloud/DeptProcider_8001.java
@@ -2,8 +2,12 @@ package com.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient    //服务器启动后，自动注册eureka中
+@EnableDiscoveryClient  //服务
 public class DeptProcider_8001 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProcider_8001.class,args);
--- springcloud-provider-dept-8001/src/main/java/com/springcloud/controller/DeptController.java
+++ springcloud-provider-dept-8001/src/main/java/com/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.springcloud.controller;
 import com.springcloud.pojo.Dept;
 import com.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,6 +14,9 @@ import java.util.List;
 public class DeptController {
     @Autowired
     private DeptService deptService;
+
+    @Autowired
+    private DiscoveryClient client;
     @PostMapping("/addDept")
     public boolean addDept(Dept dept){
         return deptService.addDept(dept);
@@ -23,5 +28,26 @@ public class DeptController {
     @RequestMapping("/findAllDept")
     public List<Dept> findAllDept(){
         return deptService.findAllDept();
+    }
+
+    //用于公司中给其他人看该微服务的作用
+    @GetMapping("/dpet/discovery")
+    public Object discovery(){
+        //获取微服务清单，即eureka中的列表
+        List<String> services = client.getServices();
+        System.out.println("discovery=>service"+services);
+        //通过eureka中的微服务id ，取得一个微服务的信息+
+
+        List<ServiceInstance> instances = client.getInstances("SPRINGCLOU-PROVIDER-DEPT");
+        for(ServiceInstance instance:instances){
+            System.out.println(
+                    instance.getHost()+" "+
+                    instance.getPort()+" "+
+                    instance.getUri()+" "+
+                    instance.getServiceId()
+                    );
+        }
+        return this.client;
+
     }
 }
