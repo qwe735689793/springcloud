d1143ce3852000019c74f9d2663f2d441a4df8a5
qwe735689793 <735689793@qq.com>
2019-12-13 17:40:51 +0800

    Eureka p8章节学习 代码提交

---
+++ springcloud-eureka-7003/src/main/java/com/springcloud/EurekaServer_7003.java
@@ -0,0 +1,13 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer //代表是Eureka服务器启动类  用于接受别人注册进来   启动类通用格式  嗯A波XXX
+public class EurekaServer_7003 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer_7003.class,args);
+    }
+}
