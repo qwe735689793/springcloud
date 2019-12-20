a5f584da699e3b65dbd7a4f28e8768287b9f9ebd
miaohaojie <735689793@qq.com>
2019-12-20 17:26:29 +0800

    ribbon 负载均衡

---
+++ springcloud-api/src/main/java/com/springcloud/service/DeptClienSerice.java
@@ -0,0 +1,22 @@
+package com.springcloud.service;
+
+import com.springcloud.pojo.Dept;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+@FeignClient(value = "SPRINGCLOU-PROVIDER-DEPT",path="/test")
+@Component
+public interface DeptClienSerice {
+    @RequestMapping("/api/Consumer/dept/add")
+    public boolean addDept(Dept dept);
+    @RequestMapping("/api/findById/{id}")
+    public Dept findById(@PathVariable("id" )Long id);
+    @RequestMapping("/api/findAllDept")
+    public List<Dept> findAllDept();
+}
